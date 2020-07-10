ee41412f4951d072466eb9c79059eafedd0a854d
Dmitriy Dubson <d.dubson@gmail.com>
2020-07-10 18:38:36 -0400

    Add pattern_matching

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod basic_types;
 mod functional;
 mod threading;
 mod examples_io;
+mod pattern_matching;
 
 fn main() {
     basic_types::basic_types();
     traits::traits();
     functional::functions_as_data();
     threading::threads_and_channels();
+    pattern_matching::pattern_matching();
     examples_io::guessing_game();
 }
---
+++ src/pattern_matching.rs
@@ -0,0 +1,22 @@
+pub fn pattern_matching() {
+    let country_code = 44;
+
+    let country = match country_code {
+        44 => "UK",
+        46 => "Sweden",
+        7 => "Russia",
+        1..=999 => "Unknown", // Catch anything that's not matched to be unknown
+        _ => "Invalid" // Anything beyond 1 to 999 is Invalid, catch-all
+    };
+
+    println!("Country code matched to {}", country);
+
+    let colors = 3;
+
+    println!("{}", match colors {
+        1 => "Bright",
+        2 | 3 => "Toned Down", // <= 2 or 3 matches
+        _ if (colors % 3 == 0) => "alternating", // An if expression
+        _ => "Dark"
+    });
+}
