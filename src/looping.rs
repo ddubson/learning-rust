e3a777b9544a013760fa5a59b74cdcf4495e93c5
Dmitriy Dubson <d.dubson@gmail.com>
2020-07-11 09:05:11 -0400

    Add looping examples

---
+++ src/looping.rs
@@ -0,0 +1,34 @@
+fn for_loops() {
+    for x in 1..11 { // for 1 through 10
+        println!("x is {}", x);
+    }
+
+    for (index, x) in (30..41).enumerate() {
+        println!("{}: {}", index, x);
+    }
+}
+
+fn while_loops() {
+    let mut x = 1;
+
+    while x < 1000 {
+        x *= 2;
+        println!("{}", x);
+    }
+}
+
+fn infinite_loops() {
+    let mut y = 1;
+
+    loop {
+        y *= 2;
+
+        if y == 1024 { break; }
+    }
+}
+
+pub fn looping() {
+    for_loops();
+    while_loops();
+    infinite_loops();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod functional;
 mod threading;
 mod examples_io;
 mod pattern_matching;
+mod looping;
 
 fn main() {
     basic_types::basic_types();
@@ -11,5 +12,6 @@ fn main() {
     functional::functions_as_data();
     threading::threads_and_channels();
     pattern_matching::pattern_matching();
+    looping::looping();
     examples_io::guessing_game();
 }
